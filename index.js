fbf099f16f4bef9cebddedecd14d05da2205267a
Kahlil <kahliljmdavis@gmail.com>
2021-12-23 22:40:30 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Kahlil Davis';
+const welcomeMessage = 'Welcome to Flatbook, ${Kahlil Davis}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase('WELCOME TO FLATBOOK, ${KAHLIL DAVIS}!');
+const shortGreeting = 'Welcome, ${currentUser.slice(K)}!';
